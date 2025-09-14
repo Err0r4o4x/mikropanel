13ea87f70797727afef8be2f4c227fe01af2bdac
IG102 <ivanpita102@gmail.com>
2025-09-13 22:57:27 -0400

    Sincronizar BD

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SupabaseStorageProvider from "@/components/SupabaseStorageProvider";
 import EnvDiagnostic from "@/components/EnvDiagnostic";
+import SyncProvider from "@/components/SyncProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,8 +31,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <SupabaseStorageProvider>
-          <EnvDiagnostic />
-          {children}
+          <SyncProvider>
+            <EnvDiagnostic />
+            {children}
+          </SyncProvider>
         </SupabaseStorageProvider>
       </body>
     </html>
